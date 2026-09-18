fdde33858117101325f68e7b422e71352c8f19b8
“[Kavin-gk]” <[kavinganesh003@gmail.com]>
2026-09-18 18:59:29 +0530

    updated script.js file

---
+++ script.js
@@ -0,0 +1,10 @@
+// Define a function that takes two numbers
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+// Call the function and store the result
+let result = addNumbers(5, 5);
+
+// Print the result to the console
+console.log("The sum is: " + result);
